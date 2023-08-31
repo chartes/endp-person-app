b13be9900c3ceeffb4851a229449b2dd621d6e41
Lucas Terriel <ls.terriel@gmail.com>
2023-08-31 11:33:27 +0200

    test conftest with pesistent db

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,6 @@
 
 File that pytest automatically looks for in any directory.
 """
-import os
-
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
@@ -13,22 +11,15 @@ from api.database import (BASE, get_db)
 from api.main import (app)
 from api.models import User
 from api.database_utils import populate_db_process
-from api.config import BASE_DIR, settings
 
-# in local uncomment (in memory)
-# SQLALCHEMY_DATABASE_TEST_URL = "sqlite://"
-# on CI Actions
-SQLALCHEMY_DATABASE_TEST_URL = f"sqlite:///{os.path.join(BASE_DIR, 'db/endp.test.sqlite')}"
+
+SQLALCHEMY_DATABASE_TEST_URL = "sqlite://"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_TEST_URL,
     connect_args={"check_same_thread": False},
     poolclass=StaticPool,
-    #echo=True,
-    #pool_size=20,
-    #max_overflow=0,
-    #pool_timeout=300,
-    #pool_recycle=3600,
+    echo=True,
 )
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -54,6 +45,6 @@ local_session = TestingSessionLocal()
 User.add_default_user(in_session=local_session)
 # 2) add data
 populate_db_process(in_session=local_session)
-
+local_session.commit()
 
 client = TestClient(app)
